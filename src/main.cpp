188df0f07e55dd6f0517d6fb867e9824f1da64a1
s3gm3nt4ti0nf4ult <s3gm3nt4ti0nf4ult@users.noreply.github.com>
2020-12-18 20:26:38 +0100

    gitignore change

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
 void setup() {
 // write your initialization code here
+    Serial.begin(115200);
 }
 
 void loop() {
-// write your code here
+    Serial.println("Dzialaaaa");
 }
